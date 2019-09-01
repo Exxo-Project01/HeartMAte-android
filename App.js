11550fc8553881cadb771a4b233afc65f84f4647
lahirudulanjaya <laahirudulanjaya@gmail.com>
2019-09-01 14:24:07 +0530

    sound record

--- App.js
+++ App.js
@@ -5,6 +5,7 @@
  * @format
  * @flow
  */
+import { Button } from 'react-native';
 
 import React, {Fragment} from 'react';
 import {
@@ -16,59 +17,107 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
+    import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+
+
+    var audioRecorderPlayer;
+class  App extends React.Component {
+  
+  constructor(){
+    super()
+
+    this.state={
+      recordSecs:0
+    }
+    audioRecorderPlayer =new AudioRecorderPlayer();
+
+  }
+  
+
+  onStartRecord = async () => {
+    const result = await audioRecorderPlayer.startRecorder();
+    audioRecorderPlayer.addRecordBackListener((e) => {
+      this.setState({
+        recordSecs: e.current_position,
+        recordTime: audioRecorderPlayer.mmssss(Math.floor(e.current_position)),
+      });
+      return;
+    });
+    console.log(result);
+  }
+  
+  onStopRecord = async () => {
+    const result = await audioRecorderPlayer.stopRecorder();
+    audioRecorderPlayer.removeRecordBackListener();
+    this.setState({
+      recordSecs: 0,
+    });
+    console.log(result);
+  }
+  
+  onStartPlay = async () => {
+    console.log('onStartPlay');
+    const msg = await audioRecorderPlayer.startPlayer();
+    console.log(msg);
+    audioRecorderPlayer.addPlayBackListener((e) => {
+      if (e.current_position === e.duration) {
+        console.log('finished');
+        this.audioRecorderPlayer.stopPlayer();
+      }
+      this.setState({
+        currentPositionSec: e.current_position,
+        currentDurationSec: e.duration,
+        playTime: audioRecorderPlayer.mmssss(Math.floor(e.current_position)),
+        duration: audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+      });
+      return;
+    });
+  }
+  
+  onPausePlay = async () => {
+    await audioRecorderPlayer.pausePlayer();
+  }
+  
+  onStopPlay = async () => {
+    console.log('onStopPlay');
+    audioRecorderPlayer.stopPlayer();
+    audioRecorderPlayer.removePlayBackListener();
+  }
+ 
+render(){
   return (
-    <Fragment>
-  <Text>Heart Mate</Text>
-    </Fragment>
+    
+  <View>
+<Button
+    onPress={this.onStartRecord}
+    title="Start Recording"
+    color="#841584"
+    accessibilityLabel="Learn more about this purple button"
+  />  
+  <Button
+    onPress={this.onStopRecord}
+    title="Stop Recording"
+    color="#841584"
+    accessibilityLabel="Learn more about this purple button"
+  /> 
+  <Button
+    onPress={this.onStartPlay}
+    title="Start Playing"
+    color="#841584"
+    accessibilityLabel="Learn more about this purple button"
+  /> 
+  <Button
+    onPress={this.onStopPlay}
+    title="Stop Playing"
+    color="#841584"
+    accessibilityLabel="Learn more about this purple button"
+  /> 
+</View>
   );
 };
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+
 
 export default App;
